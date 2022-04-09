53f10069b672bfb12a31a0552c6365cb8d65e970
tombch <tombrier@hotmail.co.uk>
2022-04-09 12:06:43 +0100

    Major speedup (with no multiprocessing) by iterating through reads directly, instead of pileup

--- README.md
+++ README.md
@@ -13,18 +13,37 @@ $ python basecount.py --bam BAM_PATH --ref REFERENCE_NAME
 
 Example output:
 ```
-reference_pos  num_reads  num_a  num_c  num_g  num_t  num_skip_del  pc_a    pc_c    pc_g    pc_t    pc_skip_del  entropy  entropy_per_read
-24             387        0      0      0      387    0             0.0     0.0     0.0     100.0   0.0          0.0      0.0
-25             407        407    0      0      0      0             100.0   0.0     0.0     0.0     0.0          0.0      0.0
-26             409        409    0      0      0      0             100.0   0.0     0.0     0.0     0.0          0.0      0.0
-27             413        0      412    0      1      0             0.0     99.758  0.0     0.242   0.0          0.011    0.0
-28             419        416    0      0      0      3             99.284  0.0     0.0     0.0     0.716        0.026    0.0
-29             421        421    0      0      0      0             100.0   0.0     0.0     0.0     0.0          0.0      0.0
-30             423        421    0      2      0      0             99.527  0.0     0.473   0.0     0.0          0.019    0.0
-31             425        0      423    1      0      1             0.0     99.529  0.235   0.0     0.235        0.021    0.0
-32             426        0      425    0      1      0             0.0     99.765  0.0     0.235   0.0          0.01     0.0
-33             426        426    0      0      0      0             100.0   0.0     0.0     0.0     0.0          0.0      0.0
-34             426        426    0      0      0      0             100.0   0.0     0.0     0.0     0.0          0.0      0.0
+reference_position  num_reads  num_a  num_c  num_g  num_t  num_deletions  pc_a    pc_c    pc_g    pc_t    pc_deletions  entropy  entropy_per_read
+0                   0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+1                   0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+2                   0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+3                   0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+4                   0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+5                   0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+6                   0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+7                   0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+8                   0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+9                   0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+10                  0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+11                  0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+12                  0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+13                  0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+14                  0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+15                  0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+16                  0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+17                  0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+18                  0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+19                  0          0      0      0      0      0              -1      -1      -1      -1      -1            1        1
+20                  1          0      1      0      0      0              0.0     100.0   0.0     0.0     0.0           0.0      0.0
+21                  1          1      0      0      0      0              100.0   0.0     0.0     0.0     0.0           0.0      0.0
+22                  1          0      0      1      0      0              0.0     0.0     100.0   0.0     0.0           0.0      0.0
+23                  3          0      1      2      0      0              0.0     33.333  66.667  0.0     0.0           0.395    0.132
+24                  1334       0      0      0      1334   0              0.0     0.0     0.0     100.0   0.0           0.0      0.0
+25                  1396       1396   0      0      0      0              100.0   0.0     0.0     0.0     0.0           0.0      0.0
+26                  1411       1411   0      0      0      0              100.0   0.0     0.0     0.0     0.0           0.0      0.0
+27                  1429       0      1427   0      2      0              0.0     99.86   0.0     0.14    0.0           0.007    0.0
+28                  1465       1453   0      1      0      11             99.181  0.0     0.068   0.0     0.751         0.031    0.0
+29                  1472       1471   0      1      0      0              99.932  0.0     0.068   0.0     0.0           0.004    0.0
 :
 ```
 
@@ -36,13 +55,14 @@ $ python basecount.py --bam BAM_PATH --ref REFERENCE_NAME --summarise
 Example output:
 ```
 ref_name                             XXXXXXXXXX
-first_ref_pos                        24
-last_ref_pos                         29853
-avg_num_reads                        210.74
-avg_num_skip_del                     4.52
-avg_pc_skip_del                      1.89
-avg_entropy                          0.085
-avg_entropy_per_read                 0.003
+ref_length                           29903
+num_no_coverage                      282
+pc_no_coverage                       0.943
+avg_num_reads                        1649.805
+avg_num_deletions                    34.822
+avg_pc_deletions                     2.011
+avg_entropy                          0.116
+avg_entropy_per_read                 0.01
 median_entropy_tile_vector           -
 median_entropy_per_read_tile_vector  -
 ```
@@ -55,13 +75,14 @@ $ python basecount.py --bam BAM_PATH --ref REFERENCE_NAME --bed BED_PATH --summa
 Example output:
 ```
 ref_name                             XXXXXXXXXX
-first_ref_pos                        24
-last_ref_pos                         29853
-avg_num_reads                        210.74
-avg_num_skip_del                     4.52
-avg_pc_skip_del                      1.89
-avg_entropy                          0.085
-avg_entropy_per_read                 0.003
-median_entropy_tile_vector           0.0,0.082,0.113,0.102,0.0,0.095,0.071,0.081,0.0,0.101,0.071,0.099,0.067,0.088,0.09,0.098,0.104,0.0,0.0,..
-median_entropy_per_read_tile_vector  0.0,0.001,0.003,0.0,0.0,0.0,0.002,0.002,0.0,0.001,0.001,0.001,0.001,0.001,0.001,0.0,0.004,0.0,0.0,0.002..
+ref_length                           29903
+num_no_coverage                      282
+pc_no_coverage                       0.943
+avg_num_reads                        1649.805
+avg_num_deletions                    34.822
+avg_pc_deletions                     2.011
+avg_entropy                          0.116
+avg_entropy_per_read                 0.01
+median_entropy_tile_vector           0.094,0.104,0.112,0.102,0.108,0.101,0.104,0.0,0.113,0.013,0.093,0.111,0.022,0.143,0.107,0.098,..
+median_entropy_per_read_tile_vector  0.001,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,..
 ```
--- basecount.py
+++ basecount.py
@@ -3,7 +3,6 @@ import math
 import pysam
 import argparse
 import statistics
-import concurrent.futures
 
 
 # Written by Sam Nicholls as part of swell
@@ -84,93 +83,91 @@ def load_scheme(bed, clip=True):
     return new_tiles
 
 
-def calculate_region(bam, ref, start, end):
-    region = []
-    samfile = pysam.AlignmentFile(bam, "rb") # type: ignore
-    normalising_factor = (1 / math.log2(5)) # Normalises maximum entropy to 1
-    for pileupcolumn in samfile.pileup(ref, start, end, min_base_quality=0):
-        reference_pos = pileupcolumn.reference_pos # type: ignore
-        if start <= reference_pos < end:
-            base_count = {
-                'A' : 0,
-                'C' : 0,
-                'G' : 0,
-                'T' : 0,
-                'SD' : 0 # skips and deletions
-            }
-            for pileupread in pileupcolumn.pileups: # type: ignore
-                if not pileupread.is_del and not pileupread.is_refskip:
-                    # TODO: Could there be non-ACGT ?
-                    base_count[pileupread.alignment.query_sequence[pileupread.query_position].upper()] += 1
-                else:
-                    base_count['SD'] += 1
+def calculate(bam, ref):
+    samfile = pysam.AlignmentFile(bam, "rb")
+    ref_len = samfile.lengths[0]
+    reads = [(read.reference_start, read.cigartuples, read.query_alignment_sequence) for read in samfile.fetch(ref)]
+    samfile.close()
 
-            row = []
-            coverage = pileupcolumn.nsegments # type: ignore
-            base_probabilities = [count / coverage for count in base_count.values()]
+    base_counts = [{'A' : 0, 'C' : 0, 'G' : 0, 'T' : 0, 'DEL' : 0} for _ in range(ref_len)]
+    operations = {0, 1, 2} # Codes for match, insertion and deletion
+
+    for read in reads:
+        start, c_tuples, seq = read
+        # Filter out all operations except match, insertion, deletion
+        # TODO: keep it simple for now, but this probably has issues
+        c_tuples = [c_tuple for c_tuple in c_tuples if c_tuple[0] in operations] # type: ignore
+
+        ref_pos = start
+        seq_pos = 0
+        for operation, length in c_tuples:
+            # Matching
+            if operation == 0:
+                for r_p, s_p in zip(range(ref_pos, ref_pos + length), range(seq_pos, seq_pos + length)):
+                    base_counts[r_p][seq[s_p]] += 1 # type: ignore
+                seq_pos += length
+                ref_pos += length
+            # Insertion in read
+            elif operation == 1:
+                seq_pos += length
+            # Deletion in read
+            else:
+                for r_p in range(ref_pos, ref_pos + length):
+                    base_counts[r_p]['DEL'] += 1 # type: ignore
+                ref_pos += length
+    
+    data = []
+    normalising_factor = (1 / math.log2(5)) # Normalises maximum entropy to 1
+    for reference_pos, b_c in enumerate(base_counts):
+        row = []
+        coverage = sum(b_c.values()) # type: ignore
+        if coverage != 0:
+            base_probabilities = [count / coverage for count in b_c.values()]
             base_percentages = [100 * probability for probability in base_probabilities]
             entropy = normalising_factor * sum([-(x * math.log2(x)) if x != 0 else 0 for x in base_probabilities])
             entropy_per_read = entropy / coverage
+        else:
+            base_percentages = [-1 for _ in b_c.values()]
+            entropy = 1
+            entropy_per_read = 1
 
-            row.append(reference_pos)
-            row.append(coverage)
-            row.extend(base_count.values())
-            row.extend(base_percentages)
-            row.append(entropy)
-            row.append(entropy_per_read)
-            region.append(row)
+        row.append(reference_pos)
+        row.append(coverage)
+        row.extend(b_c.values())
+        row.extend(base_percentages)
+        row.append(entropy)
+        row.append(entropy_per_read)
+        data.append(row)
 
-    samfile.close()
-    return region
+    return data
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--bam', required=True, help='path to BAM file')
     parser.add_argument('--ref', required=True, help='reference name')
-    parser.add_argument('--bed', default=None, help='path to BED file') # TODO
-    parser.add_argument('--index', default=None, help='path to index file')
+    parser.add_argument('--bed', default=None, help='path to BED file')
     parser.add_argument('--summarise', default=None, action='store_true', help='display summarising stats instead of per-position stats')
     parser.add_argument('--decimal-places', default=3, type=int, help='default = 3')
-    parser.add_argument('--max-processes', default=None, type=int, help='default = number of processors that the machine has')
     args = parser.parse_args()
 
-    # Create an index if it doesn't already exist
-    if (args.index is None) and (not os.path.isfile(args.bam + '.bai')):
+    # Create an index (if it doesn't already exist) in the same dir as the BAM
+    if not os.path.isfile(args.bam + '.bai'):
         pysam.index(args.bam) # type: ignore
 
-    # Determine number of bases covered
-    # TODO: does pysam have a better way?
-    samfile = pysam.AlignmentFile(args.bam, "rb") # type: ignore
-    positions = [pileupcolumn.reference_pos for pileupcolumn in samfile.pileup(args.ref, min_base_quality=0)] # type: ignore
-    first_pos = positions[0]
-    last_pos = positions[-1]
-    samfile.close()
+    # Generate data
+    data = calculate(args.bam, args.ref)
 
-    # Divide genome into appropriate regions to execute in parallel
-    if args.max_processes is None:
-        region_size = int(last_pos / os.cpu_count())
-    else:
-        region_size = int(last_pos / args.max_processes)
-    regions = [(i, i + region_size) for i in range(0, last_pos + 1, region_size)]
-    last_region_end = regions[-1][1]
-    if last_region_end > last_pos + 1:
-        regions.pop()
-        regions[-1] = regions[-1][0], last_pos + 1
-    
-    # Generate results
-    with concurrent.futures.ProcessPoolExecutor(max_workers=args.max_processes) as executor:
-        results = {start : executor.submit(calculate_region, args.bam, args.ref, start, end) for start, end in regions}
-
-    columns = ['reference_pos', 'num_reads', 'num_a', 'num_c', 'num_g', 'num_t', 'num_skip_del','pc_a', 'pc_c', 'pc_g', 'pc_t', 'pc_skip_del', 'entropy', 'entropy_per_read']
-    summary_columns = ['ref_name', 'first_ref_pos', 'last_ref_pos', 'avg_num_reads', 'avg_num_skip_del', 'avg_pc_skip_del', 'avg_entropy', 'avg_entropy_per_read', 'median_entropy_tile_vector', 'median_entropy_per_read_tile_vector']
+    columns = ['reference_position', 'num_reads', 'num_a', 'num_c', 'num_g', 'num_t', 'num_deletions','pc_a', 'pc_c', 'pc_g', 'pc_t', 'pc_deletions', 'entropy', 'entropy_per_read']
+    summary_columns = ['ref_name', 'ref_length', 'num_no_coverage', 'pc_no_coverage', 'avg_num_reads', 'avg_num_deletions', 'avg_pc_deletions', 'avg_entropy', 'avg_entropy_per_read', 'median_entropy_tile_vector', 'median_entropy_per_read_tile_vector']
 
     if args.summarise is None:
         # Print all results (in the correct order)
         print('\t'.join(columns))
-        for start, _ in regions:
-            print('\n'.join(['\t'.join([str(round(x, args.decimal_places)) for x in result]) for result in results[start].result()]))
+        for i, row in enumerate(data):
+            print('\t'.join([str(round(x, args.decimal_places)) for x in row]))
     else:
+        # Generate and display summarising statistics
         if args.bed is not None:
             scheme = load_scheme(args.bed)
             tile_starts = [tile[2]["inside_start"] for tile in scheme]
@@ -180,28 +177,31 @@ def main():
             tile_starts = []
             tile_ends = []
 
-        # Summarise results
-        # TODO: Averages currently taken over the number of positions covered by reads, NOT the length of the reference
         num_reads_column = columns.index('num_reads')
-        skips_deletions_column = columns.index('num_skip_del')
-        percentages_skips_deletions_column = columns.index('pc_skip_del')
+        num_deletions_column = columns.index('num_deletions')
+        percentages_deletions_column = columns.index('pc_deletions')
         entropies_column = columns.index('entropy')
         entropy_per_read_column = columns.index('entropy_per_read')
+
+        ref_length = len(data)
+
+        num_no_coverage = len([row[num_reads_column] for row in data if row[num_reads_column] == 0])
+        pc_no_coverage = round(100 * num_no_coverage / ref_length, args.decimal_places)
+
+        num_reads = [row[num_reads_column] for row in data]
+        avg_num_reads = round(sum(num_reads) / ref_length, args.decimal_places)
+
+        num_deletions = [row[num_deletions_column] for row in data]
+        avg_num_deletions = round(sum(num_deletions) / ref_length, args.decimal_places)
         
-        num_reads = [result[num_reads_column] for start, _ in regions for result in results[start].result()]
-        avg_num_reads = round(sum(num_reads) / len(num_reads), args.decimal_places)
+        pc_deletions = [row[percentages_deletions_column] for row in data]
+        avg_pc_deletions = round(sum(pc_deletions) / ref_length, args.decimal_places)
 
-        skips_deletions = [result[skips_deletions_column] for start, _ in regions for result in results[start].result()]
-        avg_skips_deletions = round(sum(skips_deletions) / len(skips_deletions), args.decimal_places)
-        
-        pc_skips_deletions = [result[percentages_skips_deletions_column] for start, _ in regions for result in results[start].result()]
-        avg_pc_skips_deletions = round(sum(pc_skips_deletions) / len(pc_skips_deletions), args.decimal_places)
+        entropies = [row[entropies_column] for row in data]
+        avg_entropies = round(sum(entropies) / ref_length, args.decimal_places)
 
-        entropies = [result[entropies_column] for start, _ in regions for result in results[start].result()]
-        avg_entropies = round(sum(entropies) / len(entropies), args.decimal_places)
-
-        entropy_per_read = [result[entropy_per_read_column] for start, _ in regions for result in results[start].result()]
-        avg_entropy_per_read = round(sum(entropy_per_read) / len(entropy_per_read), args.decimal_places)  
+        entropy_per_read = [row[entropy_per_read_column] for row in data]
+        avg_entropy_per_read = round(sum(entropy_per_read) / ref_length, args.decimal_places)  
         
         entropy_data = [[] for _ in scheme]
         entropy_per_read_data = [[] for _ in scheme]
@@ -209,7 +209,7 @@ def main():
         entropy_per_read_vector = []
         for i, (start, end) in enumerate(zip(tile_starts, tile_ends)):
             for j, (entropy, entropy_p_r) in enumerate(zip(entropies, entropy_per_read)):
-                if start <= first_pos + j <= end:
+                if start <= j <= end:
                     entropy_data[i].append(entropy)
                     entropy_per_read_data[i].append(entropy_p_r)
             
@@ -233,7 +233,7 @@ def main():
         else:
             entropy_per_read_vector_str = '-'
 
-        summary_stats = [args.ref, first_pos, last_pos, avg_num_reads, avg_skips_deletions, avg_pc_skips_deletions, avg_entropies, avg_entropy_per_read]
+        summary_stats = [args.ref, ref_length, num_no_coverage, pc_no_coverage, avg_num_reads, avg_num_deletions, avg_pc_deletions, avg_entropies, avg_entropy_per_read]
 
         for name, val in zip(summary_columns, [str(x) for x in summary_stats] + [entropy_vector_str, entropy_per_read_vector_str]):
             print(name + '\t' + val)
