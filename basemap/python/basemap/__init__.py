f49de8f4cd3c735556dbdde68528c103f92d2849
tombch <tombrier@hotmail.co.uk>
2023-01-03 13:17:43 +0000

    basic structure for cli as well as import

--- basemap/python/basemap/__init__.py
+++ basemap/python/basemap/__init__.py
@@ -4,3 +4,5 @@ from .basemap import *
 __doc__ = basemap.__doc__
 if hasattr(basemap, "__all__"):
 	__all__ = basemap.__all__
+
+from .cli import run
---
+++ basemap/python/basemap/cli.py
@@ -0,0 +1,4 @@
+from basemap import sum_as_string
+
+def run():
+    print(sum_as_string(1, 1))
