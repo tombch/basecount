b3375006a1370eae1f8c3305fef5f4d0056d837a
tombch <tombrier@hotmail.co.uk>
2023-01-10 10:48:53 +0000

    api change to reflect merge of internal query_ and iquery_ rust functions

--- basemap/python/basemap/api.py
+++ basemap/python/basemap/api.py
@@ -1,4 +1,4 @@
-from .basemap import all_, query_, iquery_, parse_region_
+from .basemap import all_, query_, parse_region_
 from typing import Optional
 
 
@@ -7,7 +7,7 @@ def all(bam_path: str, mapping_quality: int = 0, base_quality: int = 0):
 
 
 def query(bam_path: str, region: str, mapping_quality: int = 0, base_quality: int = 0):
-    return query_(bam_path, region, mapping_quality, base_quality)
+    return query_(bam_path, None, region, mapping_quality, base_quality)
 
 
 def iquery(
@@ -20,7 +20,7 @@ def iquery(
     if not bai_path:
         bai_path = bam_path + ".bai"
 
-    return iquery_(bam_path, bai_path, region, mapping_quality, base_quality)
+    return query_(bam_path, bai_path, region, mapping_quality, base_quality)
 
 
 def parse_region(region: str):
